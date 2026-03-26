3787dab38102751620e13309a8d81750dadcb8e8
Trinidad Bunge <trinibunge@gmail.com>
2026-03-26 09:19:31 -0300

    archivos .txt

---
+++ proyecto-base-ut01/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import ucu.edu.aed.impl.Biblioteca;
+import ucu.edu.aed.impl.Libro;
+import ucu.edu.aed.utils.FileUtils;
+
+public class App {
+    public static void main (String[] args){
+        //Crear biblioteca
+        Biblioteca biblioteca = new Biblioteca();
+    }
+}
