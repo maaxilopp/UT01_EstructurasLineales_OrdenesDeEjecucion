e98165ccf347b59b64b9c7ea033f889849bb7281
Trinidad Bunge <trinibunge@gmail.com>
2026-03-26 12:45:36 -0300

    Main

--- proyecto-base-ut01/src/main/java/org/example/App.java
+++ proyecto-base-ut01/src/main/java/org/example/App.java
@@ -8,5 +8,21 @@ public class App {
     public static void main (String[] args){
         //Crear biblioteca
         Biblioteca biblioteca = new Biblioteca();
+
+        //Cargar los libros desde adquisiciones.txt
+        FileUtils.leerLineas("src/main/java/resources/adquisiciones.txt", linea -> {
+            String[] partes = linea.split(",");
+            String titulo = partes[0];
+            String codigo = partes[1];
+            double precio = Double.parseDouble(partes[2]);
+            int stock = Integer.parseInt(partes[3]);
+
+            Libro libro = new Libro(titulo, codigo, precio, stock);
+            biblioteca.agregarLibro(libro);
+        });
+
+        FileUtils.leerLineas("src/main/java/resources/adquisiciones.txt", linea ->){
+
+        }
     }
 }
