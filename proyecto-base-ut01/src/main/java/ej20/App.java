7822224f72d6edffaede0f591f764959193ce1b8
Trinidad Bunge <trinibunge@gmail.com>
2026-04-08 21:02:17 -0300

    ej 20

--- proyecto-base-ut01/src/main/java/ej20/App.java
+++ proyecto-base-ut01/src/main/java/ej20/App.java
@@ -1,7 +1,45 @@
 package ej20;
+import ucu.edu.aed.utils.FileUtils;
+
+
 
 public class App {
-    public static void main(String[] args){System.out.println("Hola");}
+    public static void main(String[] args) {
+        pruebaUno();
+        pruebaDos();
+        pruebaTres();
+        pruebaCuatro();
+    }
+    private static void pruebaUno(){
+        RegistroCiudades registro = new RegistroCiudades();
+        FileUtils.leerLineas("ej20/suc1.txt", registro::agregarUnaSucursal);
+        System.out.println("Resultados de prueba uno: " + registro.indicarCantSucursales() + "\n");
+
+    }
+
+    private static void pruebaDos(){
+        RegistroCiudades registro = new RegistroCiudades();
+        FileUtils.leerLineas("ej20/suc1.txt", registro::agregarUnaSucursal);
+        registro.quitarSucursal("Chicago");
+        System.out.println("Resultado de prueba dos:  Shenzhen, aca esta el listado: " + registro.listarSucursales()+ "\n");
+    }
+     private static void pruebaTres (){
+         RegistroCiudades registro = new RegistroCiudades();
+         FileUtils.leerLineas("ej20/suc2.txt", registro::quitarSucursal);
+         registro.quitarSucursal("Shenzhen");
+         registro.quitarSucursal("Tokio");
+         System.out.println("Resultado de la prueba 3: Queda vacía y no da error de ejecución (respuesta d: ninguna de las anteriores)");
+         System.out.println("Cantidad de ciudades: " + registro.indicarCantSucursales()+"\n");
+
+     }
+
+    private static void pruebaCuatro (){
+        RegistroCiudades registro = new RegistroCiudades();
+        FileUtils.leerLineas("ej20/suc3.txt", linea -> registro.agregarUnaSucursal(linea));
+        System.out.print("Resultado de la prueba 4: ");
+        registro.imprimir(";");
+
+    }
 
 
 }
--- proyecto-base-ut01/src/main/java/ej20/RegistroCiudades.java
+++ proyecto-base-ut01/src/main/java/ej20/RegistroCiudades.java
@@ -3,7 +3,13 @@ import java.util.LinkedList;
 
 
 public class RegistroCiudades {
-    LinkedList<String> sucursales = new LinkedList<>();
+
+    private LinkedList<String> sucursales;
+
+    public RegistroCiudades() {
+        sucursales = new LinkedList<>();
+    }
+
 
     public void agregarUnaSucursal(String sucursal){
         sucursales.add(sucursal);
@@ -20,12 +26,21 @@ public class RegistroCiudades {
     public LinkedList<String> listarSucursales(LinkedList<String> sucursales){
         return sucursales;
     }
+
     public int indicarCantSucursales(){
         return sucursales.size();
 
      }
      public boolean estaVacio(){
         return sucursales.isEmpty();
+     }
+
+     public void imprimir(String separador){
+         for (String sucursale : sucursales) {
+             System.out.print(sucursale + separador);
+
+
+         }
      }
 
 
