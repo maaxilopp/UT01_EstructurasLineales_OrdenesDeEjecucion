09025e90f9725f36298f59c005249b36eeb73619
Maximiliano López <maximiliano.lopez@correo.ucu.edu.uy>
2026-04-11 23:03:32 -0300

    metodos completos de conjunto

--- proyecto-base-ut01/src/main/java/ucu/edu/aed/impl/ConjuntoEnlazado.java
+++ proyecto-base-ut01/src/main/java/ucu/edu/aed/impl/ConjuntoEnlazado.java
@@ -7,24 +7,22 @@ public class ConjuntoEnlazado<T> extends ListaEnlazada <T> implements TDAConjunt
         ConjuntoEnlazado<T> conjunto = new ConjuntoEnlazado<>();
         int i = 0;
         int j = 0;
-        while(i < this.tamaño() && j < otro.tamaño()){
-            if(this.obtener(i) == otro.obtener(j)) {
+        while (i < this.tamaño() && j < otro.tamaño()) {
+            if (this.obtener(i) == otro.obtener(j)) {
                 conjunto.agregar(this.obtener(i));
                 i++;
                 j++;
-            }
-            else if(((Comparable<T>)this.obtener(i)).compareTo(otro.obtener(j)) < 0){
-                    conjunto.agregar(this.obtener(i));
-                    i++;
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) < 0) {
+                conjunto.agregar(this.obtener(i));
+                i++;
 
-            }
-            else if(((Comparable<T>)this.obtener(i)).compareTo(otro.obtener(j)) > 0) {
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) > 0) {
                 conjunto.agregar(otro.obtener(j));
-                j ++;
+                j++;
             }
 
         }
-        while(i < this.tamaño()){
+        while (i < this.tamaño()) {
             conjunto.agregar(this.obtener(i));
             i++;
         }
@@ -38,16 +36,63 @@ public class ConjuntoEnlazado<T> extends ListaEnlazada <T> implements TDAConjunt
 
     @Override
     public TDAConjunto<T> interseccion(TDAConjunto<T> otro) {
-        return null;
+        ConjuntoEnlazado<T> conjunto = new ConjuntoEnlazado<>();
+        int i = 0;
+        int j = 0;
+        while (i < this.tamaño() && j < otro.tamaño()) {
+            if (this.obtener(i) == otro.obtener(j)) {
+                conjunto.agregar(this.obtener(i));
+                i++;
+                j++;
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) < 0) {
+                i++;
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) > 0) {
+                j++;
+            }
+        }
+        return conjunto;
     }
 
     @Override
     public TDAConjunto<T> diferencia(TDAConjunto<T> otro) {
-        return null;
+        ConjuntoEnlazado<T> conjunto = new ConjuntoEnlazado<>();
+        int i = 0;
+        int j = 0;
+        while (i < this.tamaño() && j < otro.tamaño()) {
+            if (this.obtener(i) == otro.obtener(j)) {
+                i++;
+                j++;
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) < 0) {
+                conjunto.agregar(this.obtener(i));
+                i++;
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) > 0) {
+                j++;
+            }
+        }
+        while (i < this.tamaño()) {
+            conjunto.agregar(this.obtener(i));
+            i++;
+        }
+        return conjunto;
     }
 
     @Override
     public boolean esSubconjuntoDe(TDAConjunto<T> otro) {
-        return false;
+        ConjuntoEnlazado<T> conjunto = new ConjuntoEnlazado<>();
+        int i = 0;
+        int j = 0;
+        while (i < this.tamaño() && j < otro.tamaño()) {
+            if (this.obtener(i) == otro.obtener(j)) {
+                conjunto.agregar(this.obtener(i));
+                i++;
+                j++;
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) < 0) {
+                i++;
+            } else if (((Comparable<T>) this.obtener(i)).compareTo(otro.obtener(j)) > 0) {
+                j++;
+            }
+        }
+        return(conjunto.tamaño()== this.tamaño()); // retornará true si conjunto a esta contenido en b xq el conjunto a tiene el mismo tamaño que el conjunto resultante.
     }
 }
+
